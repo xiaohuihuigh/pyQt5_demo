293d6e0de1d177a4556124410826c99b302911cc
zhaoyinghui <18717899732@163.com>
2023-02-24 13:02:26 +0800

    Update README.md

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ import demo
 import sys
 import threading
 
-
 # 可以将监听的事件与每一次监听发出的信号进行连接，但是由于不知道被监听事件需要多长的时间运行，所以无法准确标记每次监听的间隔
-# 监听线程可以用与监听多个线程，主要用于管理线程的运行和停止
+# 监听线程可以用于监听多个线程，主要用于管理线程的运行和停止
 # 如果监听间隔较短，会导致事件线程阻塞
+# 不在一个线程的QObject不能设置父子关系，所以监听线程和被监听的事件线程不能设置父子关系
+
+#监听线程和被监听线程共同实现了累加操作，被监听线程中的事件只是一个单次的加法操作。这里实现的是只要开始了被监听的线程，并且不中断不
+#不终止就不断调用被监听线程中的事件
 
 # 将槽函数搞到一个线程
 class Monitor(QObject):
     signal = pyqtSignal()  # 开启监听线程的信号
-    monitor_signal = pyqtSignal()  # 在监听的线程的信号信息，需要监听就一直发信号
+    monitor_signal = pyqtSignal()  # 通过发出信号确保被监听事件不断发生
 
     def __init__(self):
         super(Monitor, self).__init__()
@@ -51,7 +54,7 @@ class EventObj(QObject):
         self.function(*self.args, **self.kwargs)
 
 
-class Add():
+class Add:
     def __init__(self):
         self.n = 1
 
@@ -67,20 +70,25 @@ class DemoWidget(QWidget):
         super(DemoWidget, self).__init__(parent)
         self.ui = demo.Ui_Form()
         self.ui.setupUi(self)
+
+        #监听线程，实现对被监听线程的开始中断和终止
         self.monitorObj = Monitor()
         self.monitorThread = QThread()
         self.monitorThread.start()
         self.monitorObj.moveToThread(self.monitorThread)
         self.monitorObj.monitor_signal.connect(self.monitored)
-        # self.monitorObj = self.eventObj.parent()
 
-    def creat_event(self):
-        self.addnum = Add()  # 要监听的事件，实现在一个对象中的累加，通常情况下有一个初始值，后续对初始值进行修改
-        self.eventObj = EventObj(self.addnum.add)
+        #被监听的事件线程，实现数字累加，并且中断后不会从头累加
+        self.accumulation = Add()  # 要监听的事件，实现在一个对象中的累加，通常情况下有一个初始值，后续对初始值进行修改
+        self.eventObj = EventObj(self.accumulation.add)
+        self.eventThread = QThread()
+
+    def create_event(self):
+        self.accumulation = Add()  # 要监听的事件，实现在一个对象中的累加，通常情况下有一个初始值，后续对初始值进行修改
+        self.eventObj = EventObj(self.accumulation.add)
         self.eventThread = QThread()
         self.eventThread.start()
         self.eventObj.moveToThread(self.eventThread)
-        # self.eventObj.setParent(self.monitorObj)    #监听线程结束时，被监听的线程同样要停止，设置eventObj的parent为MonitorObj
 
     @pyqtSlot()
     def monitored(self):
@@ -91,9 +99,9 @@ class DemoWidget(QWidget):
         self.monitorObj.start = True
         self.monitorObj.pause = False
         if self.monitorObj.stop:
-            self.creat_event()
+            self.create_event()
             self.monitorObj.stop = False
-            self.monitorObj.signal.emit()
+            self.monitorObj.signal.emit() # 开启监听线程
         print(self.monitorObj.start)
 
     @pyqtSlot()
